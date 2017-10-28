7bbf6b2726e9b64da47fe1f73f4123088a008942
Pavlo Tyshevskyi <ptyshevs@student.unit.ua>
2017-10-28 14:14:10 +0300

    strlen add comments, refactoring

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,26 @@
 /*   By: ptyshevs <ptyshevs@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/26 11:43:47 by ptyshevs          #+#    #+#             */
-/*   Updated: 2017/10/26 11:48:38 by ptyshevs         ###   ########.fr       */
+/*   Updated: 2017/10/28 14:13:24 by ptyshevs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** @brief      compute length of string
+**
+** @param      str   The string
+**
+** @return     string length
+*/
+
 size_t		ft_strlen(const char *str)
 {
-	size_t	size;
+	size_t	len;
 
-	size = 0;
-	while (str[size])
-		size++;
-	return (size);
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
 }
