0016ff63be919592db8f98f6f9d53023019c430c
Pavlo Tyshevskyi <ptyshevs@student.unit.ua>
2017-10-28 17:31:34 +0300

    to_upper

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,25 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ptyshevs <ptyshevs@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/26 11:16:46 by ptyshevs          #+#    #+#             */
-/*   Updated: 2017/10/26 11:49:37 by ptyshevs         ###   ########.fr       */
+/*   Created: 2017/10/28 17:18:00 by ptyshevs          #+#    #+#             */
+/*   Updated: 2017/10/28 17:24:26 by ptyshevs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+/*
+** @brief      Convert lowercase letter representation to upper-case letter if
+**             one exists; otherwise argument is returned unchanged
+**
+** @param      c     unsigned char representation of character constant
+**
+** @return     corresponding upper-case version if one exists
+*/
 
-# include <stdlib.h>
-# include <string.h>
-# include <stdio.h> // Remove when submitting
-
-size_t		ft_strlen(const char *str);
-
-int			ft_toupper(int c);
-int			ft_tolower(int c);
-
-#endif
+int		ft_toupper(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (c - 32);
+	else
+		return (c);
+}
