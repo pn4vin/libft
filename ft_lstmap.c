addc2414d6cdb123c73f5b3c6a3311a6324648b4
Paul <ptyshevs@student.unit.ua>
2017-11-07 21:13:39 +0200

    bonus part

---
+++ ft_lstmap.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptyshevs <ptyshevs@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/07 18:24:00 by ptyshevs          #+#    #+#             */
+/*   Updated: 2017/11/07 18:26:56 by ptyshevs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** @brief      Create a new list where each element is the result of function f
+**             call
+**
+** @param      lst   The list
+** @param      f     Function to generate new element
+**
+** @return     Brand new list
+*/
+
+t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list *new_lst;
+
+	new_lst = NULL;
+	if (lst != NULL && f != NULL)
+	{
+		new_lst = f(lst);
+		lst = lst->next;
+		while (lst != NULL)
+		{
+			ft_lstadd(&new_lst, f(lst));
+			lst = lst->next;
+		}
+	}
+	return (new_lst);
+}
